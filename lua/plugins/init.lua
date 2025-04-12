3a2c0f92418765e0090c2ed5f6100e1479950d70
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-04-12 17:38:19 -0400

    adding plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,5 +6,6 @@ return {
 	{ import = "plugins.fugitive" },
     { import = "plugins.lualine" },
     { import = "plugins.lsp" },
+    { import = "plugins.vimbegood"},
 }
 
---
+++ lua/plugins/vimbegood.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ThePrimeagen/vim-be-good",
+    cmd = "VimBeGood", -- lazy-load on command
+    keys = {
+      { "<leader>vb", "<cmd>VimBeGood<CR>", desc = "Start VimBeGood" }
+    },
+  }
+}
+
