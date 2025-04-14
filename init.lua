2355d1f03ffe93b788f110f36eed6300f5983077
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-04-13 23:02:33 -0400

    Relative line numbers for netwr while :Ex dir

--- init.lua
+++ init.lua
@@ -4,3 +4,9 @@ require("config.lazy")
 -- Enable true color support
 vim.opt.termguicolors = true
 
+vim.cmd [[
+  augroup NetrwLineNumbers
+    autocmd!
+    autocmd FileType netrw setlocal number relativenumber
+  augroup END
+]]
