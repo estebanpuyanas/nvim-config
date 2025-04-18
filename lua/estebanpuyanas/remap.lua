156cafac91ecc77b028a5c4ddcbac1010d29ff4f
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-04-17 20:56:21 -0400

    adding nvimtree remap

--- lua/estebanpuyanas/remap.lua
+++ lua/estebanpuyanas/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 vim.keymap.set("n", "<leader>tt", ":vsplit | terminal<CR>", { desc = "Vertical terminal" })
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
