180c3f1ccbac4f35200822f2f47c017178b45c79
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-05-11 18:09:04 -0400

    Remapped esc in insert mode to jj

--- lua/estebanpuyanas/remap.lua
+++ lua/estebanpuyanas/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 vim.keymap.set("n", "<leader>tt", ":split | terminal<CR>", { desc = "Horizontal terminal" })
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Enter normal mode from insert mode"})
