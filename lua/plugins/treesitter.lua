d78241e03140ae16b9b2b4c037249ad94e6dfc15
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-10-02 12:57:45 -0400

    fixed issue with  not being able to source the  module from . Updating information having the plugin pull from master branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+
+    branch = "master",
+    lazy = false,
     build = ":TSUpdate",
+
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = { "java", "cpp", "python", "elixir", "dockerfile",
