19b316681ac77963de19e77bca8a67d69907549e
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-12-03 20:00:09 -0500

    blocked arrow keys while using vim

--- lua/estebanpuyanas/remap.lua
+++ lua/estebanpuyanas/remap.lua
@@ -1,3 +1,4 @@
+-- Leader key + couple remaps:
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 vim.keymap.set("n", "<leader>tt", ":split | terminal<CR>", { desc = "Horizontal terminal" })
@@ -5,3 +6,19 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file ex
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Enter normal mode from insert mode"})
 vim.keymap.set("n", "zz", ":wq!<CR>", { desc = "write-quit-bang remap" })
 vim.keymap.set("n", "ww", ":w<CR>", { desc = "normal write remap" })
+
+-- Disable arrow keys in all modes:
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({ "n", "i", "v", "x", "o" }, "<Up>", "<Nop>", opts)
+vim.keymap.set({ "n", "i", "v", "x", "o" }, "<Down>", "<Nop>", opts)
+vim.keymap.set({ "n", "i", "v", "x", "o" }, "<Left>", "<Nop>", opts)
+vim.keymap.set({ "n", "i", "v", "x", "o" }, "<Right>", "<Nop>", opts)
+
+--[[
+Alternative way of blocking arrow keys that make it annoying:
+vim.keymap.set({ "n", "i", "v" }, "<Up>", function() print("Use k!") end)
+vim.keymap.set({ "n", "i", "v" }, "<Down>", function() print("Use j!") end)
+vim.keymap.set({ "n", "i", "v" }, "<Left>", function() print("Use h!") end)
+vim.keymap.set({ "n", "i", "v" }, "<Right>", function() print("Use l!") end)
+--]]
